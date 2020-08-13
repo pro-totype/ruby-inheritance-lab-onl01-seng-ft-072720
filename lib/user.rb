55b4275775b534ed11d36e51d097ece4cdc77842
Sean B <butts.sean01@gmail.com>
2020-08-13 21:46:42 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,18 @@
 class User 
 
   def name(first_name,last_name)
-end
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def first_name
+    @first_name
+  end
+  
+  def last_name
+    @last_name
+  end
+  
 
 
 

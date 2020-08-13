12470ff24486da49062ace39caa591fc4a66baf0
Sean B <butts.sean01@gmail.com>
2020-08-13 21:56:04 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,18 +1,24 @@
 class User 
   
   attr_accessor :user 
+  
+  @@all = []
 
-  def initialize(user)
+  def name=(first_name,last_name)
     @first_name = first_name
     @last_name = last_name
   end
 
-  def self.first_name
-    @first_name
+  def user 
+    @@all 
+  end
+
+  def self.first_name(first_name)
+    @@all << @first_name
   end
   
-  def self.last_name
-    @last_name
+  def self.last_name(last_name)
+    @@all << @last_name
   end
   
 
